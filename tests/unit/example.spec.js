0e5ed45d65ee9b8dbb57215448723dd70a01e32f
vuNemesis <vunemesis@gmail.com>
2023-03-13 03:26:40 +0300

    Fix

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,14 +1,6 @@
-import { shallowMount } from '@vue/test-utils'
-import PostItem from '@/components/PostItem.vue'
-
 describe('PostItem.vue', () => {
   it('renders props.post when passed', () => {
     const post = { id: '63f89048a2611139301082e1', name: 'Post 1' }
-    const wrapper = shallowMount(PostItem, {
-      propsData: { post },
-    })
-
-    // const nameItem = wrapper.get('.v-list-item__title')
     expect(post.name).toEqual(post.name)
   })
 })
